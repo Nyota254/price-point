bb2b88ec0a831c93bfd168e75c86172c77d46eaa
Nyota254 <knyota66@gmail.com>
2020-05-05 16:05:34 +0300

    add models and register in admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import (Category,
+                     SubCategory,
+                     Sub_SubCategory,
+                     Product,
+                     Brands,
+                     ProductWithPrice,
+                     OnlineSites,
+    
+)
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Sub_SubCategory)
+admin.site.register(Sub_SubCategory)
+admin.site.register(OnlineSites)
+admin.site.register(Product)
+admin.site.register(Brands)
+admin.site.register(ProductWithPrice)
--- main/models.py
+++ main/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     '''
     category = models.CharField(max_length=60)
     
+    def __str__(self):
+        return f'{self.category} Main'
+    
     
 class SubCategory(models.Model):
     '''
@@ -14,6 +17,9 @@ class SubCategory(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     subcategory = models.CharField(max_length=60)
     
+    def __str__(self):
+        return f'{self.subcategory} of {self.category.category}'
+    
     
 class Sub_SubCategory(models.Model):
     '''
@@ -21,6 +27,9 @@ class Sub_SubCategory(models.Model):
     '''
     subcategory = models.ForeignKey(SubCategory,on_delete=models.CASCADE)
     sub_subcategory = models.CharField(max_length=60)
+    
+    def __str__(self):
+        return f'{self.sub_subcategory} of {self.subcategory.subcategory} of {self.subcategory.category.category}'
     
 
 class OnlineSites(models.Model):
@@ -31,12 +40,18 @@ class OnlineSites(models.Model):
     online_site = models.CharField(max_length=50)
     international = models.BooleanField(default=False)
     
+    def __str__(self):
+        return f'{self.online_site } site info'
+    
 class Brands(models.Model):
     '''
     Brands
     '''
     brand_pic = models.ImageField(upload_to='brandimage/')
     brand_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f'{self.brand_name} info'
     
 class Product(models.Model):
     '''
@@ -46,11 +61,17 @@ class Product(models.Model):
     product = models.CharField(max_length=60)
     product_brand = models.ForeignKey(Brands,on_delete=models.CASCADE)
     
+    def __str__(self):
+        return f'{self.product} info'
+    
 class ProductWithPrice(models.Model):
     '''
     Product itself
     '''
     sub_subcategory = models.ForeignKey(Sub_SubCategory,on_delete=models.CASCADE)
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    online_site_ = models.ForeignKey(OnlineSites,on_delete=models.CASCADE)
+    online_site = models.ForeignKey(OnlineSites,on_delete=models.CASCADE)
     price = models.IntegerField()
+    
+    def __str__(self):
+        return f'{self.product.product} Price from {self.online_site.online_site}'
