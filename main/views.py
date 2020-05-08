0ddb4b0df8c439d365b86b18ce08ba9009804ec3
Nyota254 <knyota66@gmail.com>
2020-05-08 18:58:27 +0300

    add subcategory page and activate home link

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
 from .models import (Brand,
                      Category,
                      SubCategory,
@@ -22,3 +24,23 @@ def Index_view(request):
     }
     
     return render(request,"main/index.html",context)
+
+def SubCategoryView(request, subcategory_id):
+    '''
+    Sub category
+    Arg:
+        sub-category
+    '''
+    title = "sub-category"
+    try:
+        subcategory = SubCategory.objects.get(id = subcategory_id)
+    except ObjectDoesNotExist:
+        raise Http404()
+    
+    context = {
+        "subcategory":subcategory
+    }
+    
+    return render(request,"main/subcategory.html",context)
+    
+    
